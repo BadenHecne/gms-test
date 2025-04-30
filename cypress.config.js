1caf6275a5e3b8d4a0bc7d25e0f1ff77a2375293
Baden Hecne <baden.hecne@gmail.com>
2025-04-30 10:33:39 -0300

    adicionando mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -7,11 +7,17 @@ module.exports = defineConfig({
     },
     //baseUrl: 'http://127.0.0.1:8081',
     baseUrl: 'https://golden-movie-studio.vercel.app/',
-    
-    reporter: 'junit', // Define o JUnit como reporter
+    reporter: 'mochawesome', // Define o Mochawesome como reporter
     reporterOptions: {
-      mochaFile: 'cypress/reports/junit-[hash].xml', // Diretório para salvar os relatórios XML
-      toConsole: true,
+      reportDir: 'cypress/reports', // Diretório onde os relatórios serão salvos
+      overwrite: true,
+      html: true, // Gera relatórios HTML
+      json: true, // Gera relatórios JSO
+
+    //reporter: 'junit', // Define o JUnit como reporter
+    //reporterOptions: {
+    //  mochaFile: 'cypress/reports/junit-[hash].xml', // Diretório para salvar os relatórios XML
+    //  toConsole: true,
     },
      video: true,
     //browser: 'edge', // Define o navegador padrão como Microsoft Edge
