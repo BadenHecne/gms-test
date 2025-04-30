0252e6c869e0302a084afe42b5b55ada9e7aac67
Baden Hecne <baden.hecne@gmail.com>
2025-04-30 11:07:59 -0300

    voltando para o JUnit viewer

--- cypress.config.js
+++ cypress.config.js
@@ -7,17 +7,10 @@ module.exports = defineConfig({
     },
     //baseUrl: 'http://127.0.0.1:8081',
     baseUrl: 'https://golden-movie-studio.vercel.app/',
-    reporter: 'mochawesome', // Define o Mochawesome como reporter
+    reporter: 'junit', // Define o JUnit como reporter
     reporterOptions: {
-      reportDir: 'cypress/reports', // Diretório onde os relatórios serão salvos
-      overwrite: true,
-      html: true, // Gera relatórios HTML
-      json: true, // Gera relatórios JSO
-
-    //reporter: 'junit', // Define o JUnit como reporter
-    //reporterOptions: {
-    //  mochaFile: 'cypress/reports/junit-[hash].xml', // Diretório para salvar os relatórios XML
-    //  toConsole: true,
+      mochaFile: 'cypress/reports/junit-[hash].xml', // Diretório para salvar os relatórios XML
+      toConsole: true,
     },
      video: true,
     //browser: 'edge', // Define o navegador padrão como Microsoft Edge
